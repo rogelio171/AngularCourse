aa35bbbe7545191d2b6e72cd404bfffa2fc886ac
Rogelio Orona <Rogelio_Orona@epam.com>
2018-05-09 16:10:00 -0500

    Add Config file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {AgmCoreModule} from '@agm/core';
+import { Config } from './config';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import {AgmCoreModule} from '@agm/core';
   ],
   imports: [
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyCT88f5iaFmgYqKcvs7HpJfV3CeYzu1aKU'
+      apiKey: new Config().getGoogleMapsApiKey()
     }),
     BrowserModule,
     FormsModule
